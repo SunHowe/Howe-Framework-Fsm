9d5accbf4621f29b8d51553acab6f8b171e3a9d3
SunHowe <tosunhowe@gmail.com>
2021-08-14 15:55:12 +0800

    fsm state

---
+++ Runtime/Exceptions/FsmStateIsNotActiveException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HoweFramework.Fsm.Exceptions
+{
+    /// <summary>
+    /// 状态机-状态未处于激活状态的异常
+    /// </summary>
+    public class FsmStateIsNotActiveException : Exception
+    {
+    }
+}
---
+++ Runtime/Implements/FsmState.cs
@@ -0,0 +1,77 @@
+﻿using HoweFramework.Fsm.Exceptions;
+
+namespace HoweFramework.Fsm.Implements
+{
+    /// <summary>
+    /// 有限状态机-状态基类
+    /// </summary>
+    public abstract class FsmState : IFsmState
+    {
+        private IFsm _fsm;
+        private IFsmOperator _fsmOperator;
+
+        protected bool isActive { get; private set; }
+
+        public void Initialize(IFsm fsm)
+        {
+            _fsm = fsm;
+            _fsmOperator = (IFsmOperator) fsm;
+            OnInitialize(fsm);
+        }
+
+        public void Dispose()
+        {
+            OnDispose(_fsm);
+            _fsm = null;
+            _fsmOperator = null;
+        }
+
+        public void Enter(IFsm fsm)
+        {
+            isActive = true;
+            OnEnter(fsm);
+        }
+
+        public void Leave(IFsm fsm)
+        {
+            isActive = false;
+            OnLeave(fsm);
+        }
+
+        public void Update(IFsm fsm, float dt)
+        {
+            OnUpdate(fsm, dt);
+        }
+
+        /// <summary>
+        /// 令所在的状态机切换到指定状态
+        /// </summary>
+        protected void ChangeState<T>() where T : class, IFsmState
+        {
+            if (!isActive)
+                throw new FsmStateIsNotActiveException();
+
+            _fsmOperator.ChangeState<T>();
+        }
+
+        protected virtual void OnInitialize(IFsm fsm)
+        {
+        }
+
+        protected virtual void OnDispose(IFsm fsm)
+        {
+        }
+
+        protected virtual void OnEnter(IFsm fsm)
+        {
+        }
+
+        protected virtual void OnLeave(IFsm fsm)
+        {
+        }
+
+        protected virtual void OnUpdate(IFsm fsm, float dt)
+        {
+        }
+    }
+}
